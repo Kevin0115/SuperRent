9e731579141aea996eb01c35de2ae9e5245361ab
Kevin0115 <kevin.choi0115@gmail.com>
2019-11-11 22:26:36 -0800

    initial create table sql added

---
+++ superrent.sql
@@ -0,0 +1,89 @@
+drop table branch;
+drop table reservation;
+drop table rental;
+drop table vehicle;
+drop table vehicle_type;
+drop table customer;
+drop table vehicle_return;
+
+create table branch (
+    branch_location varchar(20) not null,
+    branch_city varchar(20) not null,
+    primary key (branch_location, branch_city)
+);
+
+create table customer (
+    dlicense integer not null primary key,
+    cellphone integer not null,
+    customer_name varchar(50) not null,
+    customer_address varchar(50) not null
+);
+
+create table vehicle_type (
+    vtname varchar(20) not null primary key,
+    features varchar(50) not null,
+    w_rate integer not null,
+    d_rate integer not null,
+    h_rate integer not null,
+    wi_rate integer not null,
+    di_rate integer not null,
+    hi_rate integer not null,
+    k_rate integer not null
+);
+
+create table vehicle (
+    vlicense integer not null primary key,
+    make varchar(20) not null,
+    model varchar(20) not null,
+    year integer not null,
+    color varchar(20) not null,
+    odometer integer not null,
+    status varchar(20) not null,
+    vtname varchar(20) not null,
+    branch_location varchar(20) not null,
+    branch_city varchar(20) not null,
+    foreign key (branch_location, branch_city) references branch,
+    foreign key (vtname) references vehicle_type
+);
+
+create table reservation (
+    conf_no integer not null primary key,
+    vtname varchar(20) not null,
+    dlicense integer not null,
+    from_date date not null,
+    from_time time not null,
+    to_date date not null,
+    to_time time not null,
+    foreign key (vtname) references vehicle_type,
+    foreign key (dlicense) references customer
+    -- foreign key (from_date, from_time, to_date, to_time) references time_period
+;)
+
+create table rental (
+    rid integer not null primary key,
+    vlicense integer not null,
+    dlicense integer not null,
+    from_date date not null,
+    from_time time not null,
+    to_date date not null,
+    to_time time not null,
+    odometer integer not null,
+    card_name varchar(50),
+    card_no bigint not null,
+    exp_date date not null,
+    conf_no integer,
+    foreign key (vlicense) references vehicle,
+    foreign key (dlicense) references customer,
+    foreign key (conf_no) references reservation
+    -- foreign key (from_date, from_time, to_date, to_time) references time_period
+);
+
+create table vehicle_return (
+    rid integer not null primary key,
+    return_date date not null,
+    return_time time not null,
+    odometer integer not null,
+    fulltank boolean not null,
+    tank_value integer not null,
+    foreign key (rid) references rental
+);
