c7e028eca9aa08d46400631b3d80987be2ca6cac
Kevin0115 <kevin.choi0115@gmail.com>
2019-11-16 22:08:31 -0800

    Updates

---
+++ precache-manifest.361be39ad889a2eeec1ce566c0b5c43d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f14657ae20a6e1c5ab0c899599aab06",
+    "url": "/superrent/index.html"
+  },
+  {
+    "revision": "c46d044afbfc301ce0c2",
+    "url": "/superrent/static/css/2.e7ec537b.chunk.css"
+  },
+  {
+    "revision": "71e8c174d963bcfa2436",
+    "url": "/superrent/static/css/main.dbb9105d.chunk.css"
+  },
+  {
+    "revision": "c46d044afbfc301ce0c2",
+    "url": "/superrent/static/js/2.03a8d015.chunk.js"
+  },
+  {
+    "revision": "71e8c174d963bcfa2436",
+    "url": "/superrent/static/js/main.e3fdd055.chunk.js"
+  },
+  {
+    "revision": "cc8e26b89c93116d7e77",
+    "url": "/superrent/static/js/runtime-main.6da6da8e.js"
+  },
+  {
+    "revision": "da32b097f88b661132fedc0a82776bc6",
+    "url": "/superrent/static/media/logo.da32b097.png"
+  }
+]);
