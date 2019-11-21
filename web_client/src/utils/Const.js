2f3fd45fc24b4dbdd54dcc627bfa0be50b1f89cb
Kevin0115 <kevin.choi0115@gmail.com>
2019-11-21 02:00:34 -0800

    all branch rental and return reports complete

--- web_client/src/utils/Const.js
+++ web_client/src/utils/Const.js
@@ -1,6 +1,6 @@
 // API Endpoints
-export const API_BASE = 'http://localhost:8080/';
-// export const API_BASE = 'https://ec2.kevnchoi.com/';
+// export const API_BASE = 'http://localhost:8080/';
+export const API_BASE = 'https://ec2.kevnchoi.com/';
 
 // API Method
 export const POST = 'POST';
