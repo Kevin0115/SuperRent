a2d72fde58c7427ee9bf1e320ed7980cf4cd614c
Kevin0115 <kevin.choi0115@gmail.com>
2019-11-22 15:19:40 -0800

    done?

--- web_client/src/components/ReportBranch.js
+++ web_client/src/components/ReportBranch.js
@@ -2,7 +2,10 @@ import React from 'react';
 import {
   Accordion,
   Card,
-  Badge
+  Badge,
+  Form,
+  Button,
+  Alert
 } from 'react-bootstrap';
 import ReactTable from 'react-table';
 import 'react-table/react-table.css'
@@ -17,8 +20,9 @@ class ReportBranch extends React.Component {
     super(props);
     this.state = {
       // Branch Specs
-      branchLocation: 'Downtown',
-      branchCity: 'Vancouver',
+      branchLocation: null,
+      branchCity: null,
+      showAlert: false,
       // Received Content
       rentalsPerBranch: null,
       rentalsPerCategory: null,
@@ -30,19 +34,29 @@ class ReportBranch extends React.Component {
       returnsTotalRevenue: null,
       allReturns: null,
     }
+    this.getBranchReports = this.getBranchReports.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  componentDidMount() {
-    this.getBranchReports();
+  handleChange(e) {
+    this.setState({[e.target.name]: e.target.value});
   }
 
-  getBranchReports() {
+  getBranchReports(e) {
+    e.preventDefault();
     const { branchLocation, branchCity } = this.state;
     fetch(API_BASE + 'report/rental/' + branchCity + '/' + branchLocation, {
       method: GET,
     })
     .then(res => res.json())
     .then(json => {
+      if (!json.success) {
+        this.setState({
+          showAlert: true,
+          alertMessage: 'The branch you entered does not exist.'
+        })
+        setTimeout((() => this.setState({showAlert: false})), 10000);
+      }
       this.setState({
         rentalsPerCategory: json.content.branch_rentals_per_category,
         rentalsTotalCount: json.content.branch_total_rentals_count[0].total_rentals_today,
@@ -60,13 +74,22 @@ class ReportBranch extends React.Component {
       this.setState({
         returnsPerCategory: json.content.branch_returns_per_category,
         returnsTotalCount: json.content.branch_total_returns_count[0].total_returns_today,
-        returnsTotalRevenue: json.content.branch_total_returns_count[0].total_revenue_today,
+        returnsTotalRevenue: json.content.branch_total_returns_count[0].branch_revenue_today,
         allReturns: json.content.branch_all_returns
       })
+      this.clearForm();
     })
     .catch(function(error) {
       console.log(error);
     })
+  }
+
+  clearForm() {
+    this.setState({
+      branchLocation: null,
+      branchCity: null,
+    });
+    this.messageForm.reset();
   }
 
   render() {
@@ -74,14 +97,27 @@ class ReportBranch extends React.Component {
             returnsPerCategory, returnsTotalCount, allReturns, returnsTotalRevenue } = this.state;
     return (
       <div className="report">
-        <h2>Rental and Return Reports for All Branches</h2>
+        <h2>Rental and Return Reports for Branch</h2>
         <Card bg="primary" text="white" className="report-note">
           <Card.Body>
             <Card.Text>
-              Welcome to the reports page. Today's reports have been automatically fetched. Click on either 'Rental Reports' or 'Return Reports' to view either report(s) in table format.
+              Welcome to the reports page. Specify your branch to get started. Click on either 'Rental Reports' or 'Return Reports' to view either report(s) in table format.
             </Card.Text>
           </Card.Body>
         </Card>
+        <form className="form" ref={ form => this.messageForm = form } onSubmit={this.getBranchReports}>
+          <Form.Group>
+            <Form.Label>Branch Location</Form.Label>
+            <Form.Control type="text" name="branchLocation" onChange={this.handleChange} placeholder="Downtown"/>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Branch City</Form.Label>
+            <Form.Control type="text" name="branchCity" onChange={this.handleChange} placeholder="Vancouver"/>
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </form>
         <Accordion className="accordion">
           <Card>
             <Accordion.Toggle as={Card.Header} eventKey="0">
@@ -89,7 +125,7 @@ class ReportBranch extends React.Component {
             </Accordion.Toggle>
             <Accordion.Collapse eventKey="0">
               <div className="table-wrapper">
-                <h3>Total Rentals (All Branches): <Badge variant="info">{rentalsTotalCount}</Badge></h3>
+                <h3>Total Rentals for Branch: <Badge variant="info">{rentalsTotalCount}</Badge></h3>
                 <h5>All Rental Details</h5>
                 <ReactTable
                   className="table"
@@ -120,8 +156,8 @@ class ReportBranch extends React.Component {
             </Accordion.Toggle>
             <Accordion.Collapse eventKey="1">
               <div className="table-wrapper">
-                <h3>Total Returns (All Branches): <Badge variant="info">{returnsTotalCount}</Badge></h3>
-                <h3>Total Revenue (All Branches): <Badge variant="info">${returnsTotalRevenue}</Badge></h3>
+                <h3>Total Returns for Branch: <Badge variant="info">{returnsTotalCount}</Badge></h3>
+                <h3>Total Revenue for Branch: <Badge variant="info">${returnsTotalRevenue}</Badge></h3>
                 <h5>All Return Details</h5>
                 <ReactTable
                   className="table"
@@ -147,6 +183,10 @@ class ReportBranch extends React.Component {
             </Accordion.Collapse>
           </Card>
         </Accordion>
+        <Alert className="alert" show={this.state.showAlert} variant="danger" onClose={() => this.setState({showAlert: false})} dismissible>
+          <Alert.Heading>Something went wrong...</Alert.Heading>
+          {this.state.alertMessage}
+        </Alert>
       </div>
     )
   }
